143455787d85388a8c6e5c6001d021e50b749206
Lukas Lendvorsky <lukas@lendvorsky.cz>
2021-06-22 19:15:23 +0200

    buzzer properly turn off, adjustable freq

--- buzzer.c
+++ buzzer.c
@@ -1,5 +1,8 @@
 #include "ch.h" // ChibiOS
 #include "hal.h"
+#include "terminal.h"
+#include <stdio.h>
+#include "commands.h"
 
 static THD_WORKING_AREA(buzzer_thread_wa, 256);
 static THD_FUNCTION(buzzer_thread_fn, arg);
@@ -14,20 +17,19 @@ static int ADC3_PIN = 5;
 
 static void buzzer_on(void);
 static void buzzer_off(void);
+static void terminal_set_buzzer_freq(int argc, const char **argv);
 
-
-void pwm_setup(void)
+void pwm_setup(float freq)
 {
-    
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
 
-	palSetPadMode(HW_SERVO_PORT, HW_SERVO_PIN, PAL_MODE_ALTERNATE(HW_SERVO_GPIO_AF) |
-			PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUDR_FLOATING);
+	//palSetPadMode(HW_SERVO_PORT, HW_SERVO_PIN, PAL_MODE_ALTERNATE(HW_SERVO_GPIO_AF) |
+	//			                                   PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUDR_FLOATING);
 
 	HW_SERVO_TIM_CLK_EN();
 
-	TIM_TimeBaseStructure.TIM_Period = 500;
+	TIM_TimeBaseStructure.TIM_Period = (uint32_t)((float)TIM_CLOCK / freq);
 	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t)((168000000 / 2) / TIM_CLOCK) - 1;
 	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -51,18 +53,49 @@ void pwm_setup(void)
 
 void buzzer_start(void) 
 {
-    chThdCreateStatic(buzzer_thread_wa, sizeof(buzzer_thread_wa), LOWPRIO, buzzer_thread_fn, NULL);
+	terminal_register_command_callback(
+			"buzzer_freq",
+			"Set frequency of buzzer",
+			0,
+			terminal_set_buzzer_freq);
+
+
+	chThdCreateStatic(buzzer_thread_wa, sizeof(buzzer_thread_wa), LOWPRIO, buzzer_thread_fn, NULL);
+}
+
+
+static void terminal_set_buzzer_freq(int argc, const char **argv)
+{
+	if (argc == 2)
+	{
+		float f = 0;
+		if (sscanf(argv[1], "%f", &f) == EOF)
+		{
+			commands_printf("cant parse number: '%s' value.", argv[1]);
+			return;
+		};
+
+		pwm_setup(f);
+
+		commands_printf("done: ");
+	}
+	else
+	{
+		commands_printf("This command requires one argument: freq");
+	}
 }
 
 
 static void buzzer_on(void)
 {
-   TIM_Cmd(HW_SERVO_TIMER, ENABLE);
+	palSetPadMode(HW_SERVO_PORT, HW_SERVO_PIN, PAL_MODE_ALTERNATE(HW_SERVO_GPIO_AF) |
+			                                   PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUDR_FLOATING);
 }
 
 static void buzzer_off(void)
 {
-   TIM_Cmd(HW_SERVO_TIMER, DISABLE);
+   palSetPadMode(HW_SERVO_PORT, HW_SERVO_PIN, PAL_MODE_OUTPUT_PUSHPULL);
+   palClearPad(HW_SERVO_PORT, HW_SERVO_PIN);
 }
 
 
@@ -74,7 +107,7 @@ static THD_FUNCTION(buzzer_thread_fn, arg)
     (void)ADC3_PIN;
     (void)ADC3_PORT;
 
-    pwm_setup();
+    pwm_setup(1500);
 
     for(;;) {
         buzzer_on();
